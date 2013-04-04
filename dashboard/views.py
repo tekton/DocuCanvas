58b725867fb6be654a471f020b5d98613e1d2094
jason <jason@jason.(none)>
2013-04-04 10:48:51 -0700

    adding homepage dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,1 +1,9 @@
-# Create your views here.
+from django.shortcuts import render_to_response, redirect
+from django.template import RequestContext
+from django.contrib.auth.decorators import login_required
+from projects.models import *
+from projects.forms import *
+from issues.models import *
+
+def home(request):
+    return render_to_response("dashboard.html", {}, context_instance=RequestContext(request))
--- projects/views.py
+++ projects/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render_to_response, redirect
 from django.template import RequestContext
+from django.contrib.auth.decorators import login_required
 from projects.models import *
 from projects.forms import *
 from issues.models import *
 
+@login_required
 def home(request):
-
     projects = Project.objects.filter(product_owner=request.user)
-    print projects
-    return render_to_response("projects/projects.html", {'projects':projects}, context_instance=RequestContext(request))
+    return render_to_response("projects/projects.html", {'projects': projects}, context_instance=RequestContext(request))
+
 
 def project_form(request):
     if request.method == 'POST':
@@ -24,17 +25,17 @@ def project_form(request):
             if project.id:
                 return redirect('projects.views.project_overview', project.id, permanent=True)
             else:
-                return render_to_response("projects/project_form.html", {'form':form}, context_instance=RequestContext(request))
+                return render_to_response("projects/project_form.html", {'form': form}, context_instance=RequestContext(request))
     else:
         form = ProjectForm()
 
-    return render_to_response("projects/project_form.html", {'form':form}, context_instance=RequestContext(request))
+    return render_to_response("projects/project_form.html", {'form': form}, context_instance=RequestContext(request))
+
 
 def project_overview(request, project_id):
-
     try:
         project = Project.objects.get(pk=project_id)
         issues = Issue.objects.filter(project=project).order_by('-created')
     except:
         print 'project not found'
-    return render_to_response("projects/project_overview.html", {'project_O':project, "issues":issues}, context_instance=RequestContext(request))
+    return render_to_response("projects/project_overview.html", {'project_O': project, "issues": issues}, context_instance=RequestContext(request))
