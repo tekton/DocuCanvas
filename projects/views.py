c3dcd0a0de8523dba0a98100d377430d977ef133
jason <jason@jason.(none)>
2013-04-04 12:38:10 -0700

    adding Project form submit ability

--- projects/views.py
+++ projects/views.py
@@ -1,20 +1,29 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, redirect
 from django.template import RequestContext
 from projects.models import *
 from projects.forms import *
 
 
 def project_form(request):
-	if request.method == 'POST':
-		pass
-	else:
-		form = ProjectForm()
+    if request.method == 'POST':
+        project = Project()
+        form = ProjectForm(request.POST, instance=project)
+        if form.is_valid():
+            project = form.save()
 
-	return render_to_response("projects/project_form.html", {'form':form}, context_instance=RequestContext(request))
+            if project.id:
+                return redirect( 'projects.views.project_overview', project.id, permanent=True)
+            else:
+                return render_to_response("projects/project_form.html", {'form':form}, context_instance=RequestContext(request))
+    else:
+        form = ProjectForm()
 
-def project_overview(request, project_name):
+    return render_to_response("projects/project_form.html", {'form':form}, context_instance=RequestContext(request))
 
-	print project_name
-	project = Project.objects.get(name=project_name)
+def project_overview(request, project_id):
 
-	return render_to_response("projects/project_overview.html", {'project':project}, context_instance=RequestContext(request))
+    try:
+        project = Project.objects.get(pk=project_id)
+    except:
+        print 'project not found'
+    return render_to_response("projects/project_overview.html", {'project_O':project}, context_instance=RequestContext(request))
