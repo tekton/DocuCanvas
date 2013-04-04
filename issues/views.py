c3ed86e50fc0107f17f0ce20ee5179ad22952b58
Tyler Agee <tyler@pyroturtle.com>
2013-04-04 15:31:56 -0700

    updating some things to see where some errors are

--- issues/views.py
+++ issues/views.py
@@ -5,28 +5,31 @@ from projects.models import *
 from issues.models import *
 from issues.forms import *
 
+
 def issue_form(request):
-	if request.method == 'POST':
-		issue = Issue()
-		form = IssueForm(request.POST, instance=issue)
-		if form.is_valid():
-			try:
-				issue = form.save()
-			except:
-				print "It ain't gonna work"	
-			if issue.id:
-				return redirect( 'issues.views.issue_overview', issue.id, permanent=True)
-			else:
-				return render_to_response('issues/issue_form.html', {'form':form}, context_instance=RequestContext(request))
-	else:
-		form = IssueForm()
-		projects = Project()
-		print form
-	return render_to_response("issues/issue_form.html", {'form':form}, context_instance=RequestContext(request))
+    if request.method == 'POST':
+        issue = Issue()
+        form = IssueForm(request.POST, instance=issue)
+        if form.is_valid():
+            try:
+                issue = form.save()
+            except Exception, e:
+                print e
+                print form.errors
+            if issue.id:
+                return redirect('issues.views.issue_overview', issue.id, permanent=True)
+            else:
+                return render_to_response('issues/issue_form.html', {'form': form}, context_instance=RequestContext(request))
+    else:
+        form = IssueForm()
+        projects = Project()
+        print form
+    return render_to_response("issues/issue_form.html", {'form': form}, context_instance=RequestContext(request))
+
 
 def issue_overview(request, issue_id):
-	try:
-		issue = Issue.objects.get(pk=issue_id)
-	except:
-		print "Somebody messed up the issue overview"
-	return render_to_response("issues/issue_overview.html", {'issue_O':issue}, context_instance=RequestContext(request))
+    try:
+        issue = Issue.objects.get(pk=issue_id)
+    except:
+        print "Somebody messed up the issue overview"
+    return render_to_response("issues/issue_overview.html", {'issue_O': issue}, context_instance=RequestContext(request))
