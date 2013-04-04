cf4a06ec64458f333f95c6b3dda217749c2c1cfd
jason <jason@jason.(none)>
2013-04-04 15:18:29 -0700

    adding registration

--- auth/views.py
+++ auth/views.py
@@ -17,7 +17,7 @@ from django.contrib.auth.decorators import login_required
 from accounts.models import *
 
 def register(request):
-    '''
+    
     if request.method == 'POST':
         next = request.POST.get("next", "/")
         form = RegisterForm(request.POST)
@@ -30,32 +30,24 @@ def register(request):
                 return render_to_response("registration/registration.html", {'form': form, 'next': next}, context_instance=RequestContext(request))
             # log the user in before sending them to their next destination
             user = authenticate(username=request.POST.get("username"), password=request.POST.get("password1"))
-            login(request, user)
+            #login(request, user)
             ### TODO Create account here!
             account = Account()
+            account.user = User.objects.get(pk=user.id)
             account.created_by = user
-            account.original_splash_page = request.session.get('splash')
             account.save()
             #
-            if request.POST.get("video", False) is not False:
-                print "go to new campaign straight away!"
-                response = redirect('campaign.views.new_campaign')
-                print request.POST.get("video")
-                response["Location"] += "?video=" + str(request.POST.get("video"))
-            else:
-                print "there was no video posted, so just go to the url for new url"
-                response = redirect('campaign.views.campaign_url')
-            return response
+            return render_to_response("projects/project_form.html", {}, context_instance=RequestContext(request))
+            #response = redirect('auth.views.register')
+            #return response
         else:
             print "errors in registration"
             print form.errors
 
-    else:
-    '''
-        
-    print "not post"
-    form = RegisterForm()
-    next = request.GET.get("next", "/")
-    # Add CSRF context token to response.
-    return render_to_response("registration/registration.html", {'form': form, 'next': next}, context_instance=RequestContext(request))
+    else:    
+        print "not post"
+        form = RegisterForm()
+        next = request.GET.get("next", "/")
+        # Add CSRF context token to response.
+        return render_to_response("registration/registration.html", {'form': form, 'next': next}, context_instance=RequestContext(request))
     #return render_to_response("registration/registration.html", {'r_form': form, 'next': next}, context_instance=RequestContext(request))
