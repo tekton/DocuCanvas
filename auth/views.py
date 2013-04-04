4abf7f10931f885c97d23d8bfc80bffcd8d807e3
jason <jason@jason.(none)>
2013-04-04 15:34:21 -0700

    Made login

--- auth/views.py
+++ auth/views.py
@@ -30,7 +30,7 @@ def register(request):
                 return render_to_response("registration/registration.html", {'form': form, 'next': next}, context_instance=RequestContext(request))
             # log the user in before sending them to their next destination
             user = authenticate(username=request.POST.get("username"), password=request.POST.get("password1"))
-            #login(request, user)
+            login(request, user)
             ### TODO Create account here!
             account = Account()
             account.user = User.objects.get(pk=user.id)
@@ -51,3 +51,29 @@ def register(request):
         # Add CSRF context token to response.
         return render_to_response("registration/registration.html", {'form': form, 'next': next}, context_instance=RequestContext(request))
     #return render_to_response("registration/registration.html", {'r_form': form, 'next': next}, context_instance=RequestContext(request))
+
+def login_func(request):
+    next = request.POST.get("next", "/")
+    state = ""
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = authenticate(username=request.POST.get("username"), password=request.POST.get("password"))
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    state = "You're successfully logged in!"
+                else:
+                    state = "Your account is not active, please contact the site admin."
+            else:
+                state = "Your username and/or password were incorrect."
+            login(request, user)
+            return render_to_response("registration/login.html", {'a_form': form, 'next': next, 'state': state}, context_instance=RequestContext(request))
+        else:
+            #to_json = {"error": "error with username and/or password"}
+            #return HttpResponse(simplejson.dumps(to_json), mimetype='application/json', status=400)
+            return render_to_response("registration/login.html", {'a_form': form, 'next': next, 'state': state}, context_instance=RequestContext(request))
+    else:
+        form = AuthenticationForm()
+
+    return render_to_response("registration/login.html", {'a_form': form, 'next': next, 'state': state}, context_instance=RequestContext(request))
