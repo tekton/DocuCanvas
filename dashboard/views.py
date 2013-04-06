0728f2083df13e1391915bafde836fd492fb0626
jason <jason@jason.(none)>
2013-04-06 16:41:16 -0700

    adding all dropdown menus to base

--- dashboard/views.py
+++ dashboard/views.py
@@ -7,5 +7,6 @@ from issues.models import *
 
 def home(request):
     issues = Issue.objects.filter(assigned_to=request.user).order_by('-created')
+    projects = Project.objects.filter(lead_developer=request.user).order_by('-created')
     subscribed = []
-    return render_to_response("dashboard.html", {"issues":issues, "subscribed":subscribed}, context_instance=RequestContext(request))
+    return render_to_response("dashboard.html", {"issues":issues, "subscribed":subscribed, "projects":projects}, context_instance=RequestContext(request))
