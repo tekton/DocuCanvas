c36bb53a3ae6fc06640f4cc4cb4f21d2eb8d607b
Siid <siid@siid-VirtualBox.(none)>
2013-04-08 14:52:33 -0700

    added models.py to boards

--- boards/models.py
+++ boards/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Board(models.Model):
+    title = models.CharField(max_length=255)
+    image = models.CharField(max_length=255)  # image URI
+    height = models.FloatField()
+    width = models.FloatField()
+
+
+class BoardNote(models.Model):
+    board = models.ForeignKey(Board)
+    user = models.ForeignKey(User)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+
+    def __unicode__(self):
+        pass
+
+
+class BoardNode(models.Model):
+    board = models.ForeignKey(Board)
+    x = models.FloatField(default=0)
+    y = models.FloatField(default=0)
+    nodeType = models.CharField(max_length=255)
+    nodeLink = models.IntegerField(null=True, blank=True)  # Basic foreign key to nodeType
+    # Fields used for testing
+    user = models.ForeignKey(User)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
