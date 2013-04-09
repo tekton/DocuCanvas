bce41e519c8b6f7cc09e71ef976b1d3c1c969182
jason <jason@jason.(none)>
2013-04-09 09:13:55 -0700

    making navigation more intuitive

--- dashboard/views.py
+++ dashboard/views.py
@@ -11,9 +11,10 @@ def home(request):
     if request.user.is_authenticated():
         issues = Issue.objects.filter(assigned_to=request.user).order_by('-created')
         pins = PinIssue.objects.select_related().filter(user=request.user)
+
         #projects = Project.objects.filter(lead_developer=request.user).order_by('-created')
         projects = Project.objects.all().order_by('-created')
         subscribed = SubscriptionToIssue.objects.select_related().filter(user=request.user)
-        return render_to_response("dashboard.html", {"issues": issues, "subscribed": subscribed, "projects": projects, "pins": pins}, context_instance=RequestContext(request))
+        return render_to_response("dashboard.html", {"issues": issues, "subscribed": subscribed, "projects": projects, "pins": pins, "page_type": "Dashboard", "page_value": "Overview"}, context_instance=RequestContext(request))
     else:
         return redirect('auth.views.login_func')
