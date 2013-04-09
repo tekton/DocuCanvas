afe8da23320405160932ebac381e0c9dbcc18b03
jason <jason@jason.(none)>
2013-04-08 21:15:32 -0700

    adding assign and assign to to issues

--- issues/urls.py
+++ issues/urls.py
@@ -6,8 +6,9 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     (r'new', 'issues.views.issue_form'),
+    (r'assign', 'issues.views.assign'),
     (r'subscribe', 'issues.views.subscribe'),
-    (r'comment/([A-Za-z0-9_\.-]+)', 'issues.views.submit_comment'),
     (r'pin', 'issues.views.pin'),
+    (r'comment/([A-Za-z0-9_\.-]+)', 'issues.views.submit_comment'),
     (r'([A-Za-z0-9_\.-]+)', 'issues.views.issue_overview'),
 )
--- issues/views.py
+++ issues/views.py
@@ -3,18 +3,57 @@ from django.shortcuts import render_to_response, redirect
 from django.template import RequestContext
 from django.http import HttpResponse
 from django.utils import simplejson
+from django.db import models
 # from projects.models import Project
-from issues.models import Issue, IssueComment, SubscriptionToIssue
+from issues.models import Issue, IssueComment, SubscriptionToIssue, PinIssue
 from issues.forms import IssueForm, CommentForm
 from django.contrib.auth.models import User
 
 
 def pin(request):
-    return render_to_response("issues/pin.html", {'issues': issues}, context_instance=RequestContext(request))
+    to_json = {}
+    try:
+        issue = Issue.objects.get(pk=request.POST['issue'])
+        try:
+            pin = PinIssue.objects.get(user=request.user, issue=issue)
+            to_json["status"] = "You have already pinned this issue"
+        except:
+            try:
+                pin = PinIssue()
+                pin.issue = issue
+                pin.user = request.user
+                pin.save()
+                to_json["status"] = "Successfully pinned issue"
+            except Exception, e:
+                print e
+                to_json["status"] = "Error pinning"
+    except:
+        to_json["status"] = "Issue does not exist"
+    return HttpResponse(simplejson.dumps(to_json), mimetype='application/json')
 
 
 def assign(request):
-    return render_to_response("issues/pin.html", {'issues': issues}, context_instance=RequestContext(request))
+    to_json = {}
+    try:
+        issue = Issue.objects.get(pk=request.POST['issue'])
+        if issue.assigned_to:
+            to_json["status"] = "Successfully reassigned issue"
+
+        else:
+            to_json["status"] = "Successfully assigned issue"
+
+        if request.POST['assign'] == 'assign':
+            issue.assigned_to = request.user
+        else:
+            try:
+                issue.assigned_to = User.objects.get(pk=request.POST['user'])
+            except:
+                to_json["status"] = "Unable to find user"
+
+        issue.save()
+    except:
+        to_json["status"] = "Unable to assign to issue"
+    return HttpResponse(simplejson.dumps(to_json), mimetype='application/json')
 
 
 def subscribe(request):
@@ -69,11 +108,17 @@ def issue_overview(request, issue_id):
         print e
 
     try:
+        users = User.objects.all()
+    except Exception, e:
+        print "Unable to get user list"
+        print e
+
+    try:
         comment_form = CommentForm()
     except Exception, e:
         print e
 
-    return render_to_response("issues/issue_overview.html", {'issue': issue, 'comment_form': comment_form, 'comments':  comments, "page_type":"Issue"}, context_instance=RequestContext(request))
+    return render_to_response("issues/issue_overview.html", {'issue': issue, 'comment_form': comment_form, 'comments': comments, "users": users, "page_type": "Issue"}, context_instance=RequestContext(request))
 
 
 def submit_comment(request, issue_id):
