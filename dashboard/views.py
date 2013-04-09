5fe7e461c1c424ea9d69be929f2c4c5e2f596c70
jason <jason@jason.(none)>
2013-04-08 23:15:37 -0700

    adding real pin functionality

--- dashboard/views.py
+++ dashboard/views.py
@@ -10,9 +10,10 @@ def home(request):
     #issues = Issue.objects.filter(assigned_to=request.user).order_by('-created')
     if request.user.is_authenticated():
         issues = Issue.objects.all().order_by('-created')
+        pins = PinIssue.objects.select_related().filter(user=request.user)
         #projects = Project.objects.filter(lead_developer=request.user).order_by('-created')
         projects = Project.objects.all().order_by('-created')
         subscribed = []
-        return render_to_response("dashboard.html", {"issues": issues, "subscribed": subscribed, "projects": projects, "page_type": "Dashboard"}, context_instance=RequestContext(request))
+        return render_to_response("dashboard.html", {"issues": issues, "subscribed": subscribed, "projects": projects, "page_type": "Dashboard", "pins": pins}, context_instance=RequestContext(request))
     else:
         return redirect('auth.views.login_func')
