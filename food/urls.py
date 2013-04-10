0e7bb35c4a93258fb3bdb33f8ab03ca6c49af3a7
jason <jason@jason.(none)>
2013-04-10 16:28:41 -0700

    pushing food request app

---
+++ food/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('',
+    (r'new', 'food.views.food_form'),
+    (r'overview', 'food.views.food_overview'),
+    (r'comp/([A-Za-z0-9_\.-]+)', 'food.views.food_received'),
+    (r'view/([A-Za-z0-9_\.-]+)', 'food.views.user_food_overview'),
+    (r'all', 'food.views.all_food'),
+    (r'complete', 'food.views.food_overview_complete'),
+    (r'([A-Za-z0-9_\.-]+)', 'food.views.get_food'),
+)
