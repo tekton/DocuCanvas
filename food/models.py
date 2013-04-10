7cfdf0e1ac8e2a84ced48cc50019d683b904550c
jason <jason@jason.(none)>
2013-04-10 16:39:46 -0700

    commiting foooooood changes for realz

--- food/models.py
+++ food/models.py
@@ -5,9 +5,28 @@ from django.contrib import admin
 
 
 class FoodRequest(models.Model):
-    user = models.ForeignKey(User)
-    item = models.CharField(max_length=255)
+    quantity_choices = (
+        ('lbs', 'Pounds'),
+        ('qty', 'Items'),
+        ('ltr', 'Litres'),
+        ('gal', 'Gallons'),
+    )
+    user = models.ForeignKey(User)  # name of user making request
+    item = models.CharField(max_length=255)  # item being requested
+    quantity = models.FloatField(default=0)  # amount of item being requested
+    quantity_type = models.CharField(max_length=3, choices=quantity_choices, default=(1, 1))  # type of quantity ex. lbs, ltr, gal
+    request_initiated = models.DateField(auto_now_add=True, null=True, blank=True)  # when request was initated
+    desired_completion = models.DateField(blank=True, null=True)  # when user wants request completed
+    request_completed_bool = models.NullBooleanField(default=False)  # bool for whether or not request has been completed
+    request_completed_date = models.DateField(auto_now=True, blank=True, null=True)  # date request was handled
+    cost_per_quantity = models.FloatField(default=0)  # cost per unit of item requested
+    total_cost = models.FloatField(default=0)  # total cost of order (per unit price * # of units)
 
+    def __unicode__(self):
+        return self.user.username + ':' + self.item
+
+    def FindTotal(self):
+        self.total_cost = self.quantity * self.cost_per_quantity
 
 
 admin.site.register(FoodRequest)
--- food/views.py
+++ food/views.py
@@ -1,7 +1,101 @@
 # Create your views here.
 from django.shortcuts import render_to_response, redirect
 from django.template import RequestContext
-from food.forms import FoodForm
+from food.forms import FoodFormTres, FoodForm, FoodFormComplete
 from food.models import FoodRequest
 
-# def food_form(request):
+
+def food_form(request):
+    if request.method == "POST":
+        food = FoodRequest()
+        form = FoodFormTres(request.POST, instance=food)
+        try:
+            food = form.save()
+        except Exception, e:
+            print "unable to save food form"
+            print e
+        try:
+            food.FindTotal()
+        except Exception, e:
+            print "unable to find total"
+            print e
+        try:
+            food.save()
+        except Exception, e:
+            print "unable to save food"
+            print e
+        if food:
+            return redirect('food.views.get_food', food.id)
+    else:
+        form = FoodFormTres()
+    return render_to_response('food/food_form.html', {'form': form}, context_instance=RequestContext(request))
+
+
+def get_food(request, food_id):
+    try:
+        food = FoodRequest.objects.get(pk=food_id)
+    except Exception, e:
+        print "unable to find food item"
+        print e
+    return render_to_response('food/food_view.html', {'food': food}, context_instance=RequestContext(request))
+
+
+def food_received(request, food_id):
+    food = FoodRequest.objects.get(pk=food_id)
+    if request.method == "POST":
+        form = FoodFormComplete(request.POST, instance=food)
+        try:
+            food.save()
+        except Exception, e:
+            print "food went bad"
+            print e
+        print request.POST['request_completed_bool']
+        if request.POST['request_completed_bool'] == '2':
+            food.request_completed_bool = True
+            food.save()
+        elif request.POST['request_completed_bool'] != '3':
+            food.request_completed_bool = False
+            food.save()
+        if food:
+            return redirect('food.views.user_food_overview', food_id)
+    else:
+        form = FoodFormComplete(instance=food)
+    return render_to_response('food/food_comp.html', {'form': form, 'food': food}, context_instance=RequestContext(request))
+
+
+def food_overview(request):
+    try:
+        pending_food = FoodRequest.objects.filter(request_completed_bool=False).order_by('-request_initiated')
+    except Exception, e:
+        print "could not load pending requests"
+        print e
+    return render_to_response('food/food_overview.html', {'pending_food': pending_food}, context_instance=RequestContext(request))
+
+
+def user_food_overview(request, food_id):
+    food = FoodRequest.objects.get(pk=food_id)
+    user_name = food.user
+    try:
+        user_food = FoodRequest.objects.filter(request_completed_bool=False, user=user_name).order_by('-request_initiated')
+    except Exception, e:
+        print e
+        print "failed to find user's pending food"
+    return render_to_response('food/food_userview.html', {'user_food': user_food, 'user_name': user_name}, context_instance=RequestContext(request))
+
+
+def food_overview_complete(request):
+    try:
+        complete_food = FoodRequest.objects.filter(request_completed_bool=True).order_by('-request_initiated')
+    except Exception, e:
+        print "could not load finished food"
+        print e
+    return render_to_response('food/food_overview_complete.html', {'complete_food': complete_food}, context_instance=RequestContext(request))
+
+
+def all_food(request):
+    try:
+        food_all = FoodRequest.objects.all().order_by('request_completed_bool', '-request_initiated')
+    except Exception, e:
+        print e
+        print "lost all of the food"
+    return render_to_response('food/food_all.html', {'food_all': food_all}, context_instance=RequestContext(request))
