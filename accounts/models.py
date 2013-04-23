4fe48819a765933be36b046eb57d15a13b53766c
Tyler Agee <pyroturtle@gmail.com>
2013-04-23 10:58:02 -0700

    adding google table

--- accounts/models.py
+++ accounts/models.py
@@ -15,3 +15,13 @@ class Account(models.Model):
     facebook = models.CharField(max_length=255, null=True, blank=True)  #
     twitter = models.CharField(max_length=255, null=True, blank=True)  #
     organization = models.CharField(max_length=255, null=True, blank=True)  #
+
+
+class GoogleAccount(models.Model):
+    """
+        Google Account table to allow people to link 1 to unlimited Google/YouTube account to their account
+    """
+    user = models.ForeignKey(User)
+    account = models.ForeignKey(Account)
+    security_token = models.CharField(max_length=255, null=True, blank=True)
+    # put other fields needed below ?
