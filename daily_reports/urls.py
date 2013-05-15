f4de6d0831fd8e04b3b508489e74f2be53472f41
Tyler Agee <pyroturtle@gmail.com>
2013-05-15 15:53:23 -0700

    adding new daily report reporting base

--- daily_reports/urls.py
+++ daily_reports/urls.py
@@ -5,10 +5,11 @@ from django.conf.urls import *
 '''
 
 urlpatterns = patterns('',
-    url(r'edit/([0-9]{4})/([0-9]{1,2})/([0-9]{1,2})' , 'daily_reports.views.edit_report'),
-    url(r'edit$', 'daily_reports.views.edit_report'),
-    url(r'editglobal/([0-9]{4})/([0-9]{1,2})/([0-9]{1,2})' , 'daily_reports.views.edit_global_report'),
-    url(r'editglobal$', 'daily_reports.views.edit_global_report'),
-    url(r'view/([0-9]{4})/([0-9]{1,2})/([0-9]{1,2})' , 'daily_reports.views.view_reports'),
-    url(r'view$', 'daily_reports.views.view_reports'),
-)
+                       url(r'edit/([0-9]{4})/([0-9]{1,2})/([0-9]{1,2})', 'daily_reports.views.edit_report'),
+                       url(r'edit$', 'daily_reports.views.edit_report'),
+                       url(r'editglobal/([0-9]{4})/([0-9]{1,2})/([0-9]{1,2})', 'daily_reports.views.edit_global_report'),
+                       url(r'editglobal$', 'daily_reports.views.edit_global_report'),
+                       url(r'view/([0-9]{4})/([0-9]{1,2})/([0-9]{1,2})', 'daily_reports.views.view_reports'),
+                       url(r'view$', 'daily_reports.views.view_reports'),
+                       url(r'^$', 'daily_reports.views.index'),
+                       )
