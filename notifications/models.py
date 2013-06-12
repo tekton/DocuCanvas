a7f04243de6df09356e609b75f828663d2008973
jason <jason@jason-VirtualBox.(none)>
2013-06-12 15:18:36 -0700

    updating extends reference in notifications view

--- notifications/models.py
+++ notifications/models.py
@@ -6,6 +6,8 @@ class Notification(models.Model):
     creator = models.ForeignKey(User)
     message = models.CharField(max_length=255, blank=True, null=True)
     created = models.DateField(auto_now_add=True)
+    def __unicode__(self):
+        return self.message
 
 
 class NotificationRecipient(models.Model):
@@ -13,3 +15,5 @@ class NotificationRecipient(models.Model):
     notification = models.ForeignKey(Notification)
     read = models.NullBooleanField(null=True, blank=True, default=False)
     created = models.DateField(auto_now_add=True)
+    def __unicode__(self):
+        return self.notification.message
