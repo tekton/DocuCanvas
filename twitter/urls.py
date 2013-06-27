3e85fc5fa19dba4d3be15d204350d900c5c68623
RyanSharp <Ryan.Sharp@channelfactory.com>
2013-06-26 17:28:52 -0700

    Removed url to function that doesn't exist

--- twitter/urls.py
+++ twitter/urls.py
@@ -3,11 +3,10 @@ from django.conf.urls import *
 urlpatterns = patterns('',
     url(r'^add_account', 'twitter.views.add_twitter_acct'),
     url(r'^rmv_account', 'twitter.views.remove_twitter_acct'),
-    url(r'^sayhello', 'twitter.views.tweeter'),
     url(r'^newComment/([A-Za-z0-9_\.-]+)/([A-Za-z0-9_\.-]+)$', 'twitter.views.send_dm_comment_update'),
     url(r'^newIssue/([A-Za-z0-9_\.-]+)/([A-Za-z0-9_\.-]+)$', 'twitter.views.send_dm_new_issue'),
     url(r'^newIssueAll/([A-Za-z0-9_\.-]+)$', 'twitter.views.send_dm_new_issue_all'),
     url(r'^newProject/([A-Za-z0-9_\.-]+)/([A-Za-z0-9_\.-]+)$', 'twitter.views.send_dm_new_project'),
-    url(r'^newProjectAll/)([A-Za-z0-9_\.-]+)$', 'twitter.views.send_dm_new_project_all'),
+    url(r'^newProjectAll/([A-Za-z0-9_\.-]+)$', 'twitter.views.send_dm_new_project_all'),
     url(r'^newProjectTweet/([A-Za-z0-9_\.-]+)$', 'twitter.views.new_project_tweet'),
 )
