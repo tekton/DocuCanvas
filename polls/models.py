f1065692b119a75e619e4161abb3fdb605b5cb3f
RyanSharp <Ryan.Sharp@channelfactory.com>
2013-07-11 13:04:30 -0700

    updating polls.  can now create and vote on polls.

--- polls/models.py
+++ polls/models.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 # Create your models here.
 class Poll(models.Model):
     creator = models.ForeignKey(User)
-    total_items = models.IntegerField()
+    total_items = models.IntegerField(default=2, null=True, blank=True)
     name = models.CharField(max_length=255)
     context = models.CharField(max_length=255, null=True, blank=True)
     created = models.DateField(auto_now_add=True, null=True, blank=True)
@@ -16,7 +16,7 @@ class PollItem(models.Model):
     added_by = models.ForeignKey(User)
     poll = models.ForeignKey(Poll)
     item = models.CharField(max_length=255)
-    votes = models.IntegerField(default=0)
+    votes = models.IntegerField(default=0, null=True, blank=True)
     created = models.DateField(auto_now_add=True, null=True, blank=True)
 
 
@@ -25,3 +25,8 @@ class PollUser(models.Model):
     poll = models.ForeignKey(Poll)
     ip = models.IPAddressField(blank=True, null=True)
     voted = models.IntegerField(default=0)
+
+
+admin.site.register(Poll)
+admin.site.register(PollItem)
+admin.site.register(PollUser)
--- polls/urls.py
+++ polls/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import *
 
 urlpatterns = patterns('',
     (r'^new/', 'polls.views.new_poll'),
-    (r'^overview/([A-Za-z0-9_\.-]+)$', 'polls.views.poll_view'),
+    (r'^overview/([A-Za-z0-9_\.-]+)$', 'polls.views.poll_overview'),
+    (r'^vote/([A-Za-z0-9_\.-]+)$', 'polls.views.vote'),
 )
