bc6a08eb371ca999a8ca218d01b28fcaa0a17067
RyanSharp <Ryan.Sharp@channelfactory.com>
2013-07-23 15:11:34 -0700

    New search function now considers Issue Comments and Help Responses

--- search/views.py
+++ search/views.py
@@ -17,13 +17,40 @@ from helpdesknew.models import HelpRequest, HelpResponse
 
 @login_required
 def search(request):
-	try:
-		projects = Project.objects.all()
-	except Exception, e:
-		print e
+    try:
+        projects = Project.objects.all()
+    except Exception, e:
+        print e
 
-	search = request.POST['searchText']
-	q_issue = Issue.objects.filter(Q(summary__contains=search) | Q(description__contains=search))
-	q_help = HelpRequest.objects.filter(Q(name__contains=search) | Q(question__contains=search))
+    search = request.POST['searchText']
+    q_helpresponse = HelpResponse.objects.filter(Q(response__contains=search))
+    q_issuecomment = IssueComment.objects.filter(Q(description__contains=search))
+    q_issue = Issue.objects.filter(Q(summary__contains=search) | Q(description__contains=search))
+    q_help = HelpRequest.objects.filter(Q(name__contains=search) | Q(question__contains=search))
+    issue = list(q_issue)
+    response_to_issue = list()
+    for comment in q_issuecomment:
+        response_to_issue.append(comment.issue)
+    issue += response_to_issue
+    issue.sort()
+    # last_issue = issue[-1]
+    for i in range(len(issue)-2, -1, -1):
+        if last_issue == issue[i]:
+            del help[i]
+        else:
+            last_issue = issue[i]
+    help = list(q_help)
+    response_to_help = list()
+    for response in q_helpresponse:
+        response_to_help.append(response.helprequest)
+    help += response_to_help
+    help.sort()
+    last_help = help[-1]
+    for i in range(len(help)-2, -1, -1):
+        if last_help == help[i]:
+            del help[i]
+        else:
+            last_help = help[i]
 
-	return render_to_response("search/search_results.html", {'issues': q_issue, 'help_requests': q_help, "projects": projects}, context_instance=RequestContext(request))
+
+    return render_to_response("search/search_results.html", {'issues': q_issue, 'help_requests': help, "projects": projects}, context_instance=RequestContext(request))
