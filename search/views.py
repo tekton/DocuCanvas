3f650d69e97bba70f543f89b672f1743cdbc463e
RyanSharp <Ryan.Sharp@channelfactory.com>
2013-07-26 10:08:10 -0700

    Finished javascript for search results page

--- search/views.py
+++ search/views.py
@@ -1,4 +1,6 @@
 # Create your views here.
+import json
+
 from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib.auth.models import User
 from django.core.exceptions import PermissionDenied
@@ -21,37 +23,52 @@ def search(request):
         projects = Project.objects.all()
     except Exception, e:
         print e
-
-    search = request.POST['searchText']
+    search = request.POST["searchText"]
     q_helpresponse = HelpResponse.objects.filter(Q(response__contains=search))
+    json_response = {}
+    response_count = 0
+    for item in q_helpresponse:
+        json_response["id-"+str(response_count)] = item.helprequest.id
+        json_response["name-"+str(response_count)] = item.helprequest.name
+        json_response["response-"+str(response_count)] = item.response
+        response_count = response_count + 1
     q_issuecomment = IssueComment.objects.filter(Q(description__contains=search))
-    q_issue = Issue.objects.filter(Q(summary__contains=search) | Q(description__contains=search))
-    q_help = HelpRequest.objects.filter(Q(name__contains=search) | Q(question__contains=search))
-    issue = list(q_issue)
-    response_to_issue = list()
-    for comment in q_issuecomment:
-        response_to_issue.append(comment.issue)
-    issue += response_to_issue
-    issue.sort()
-    if issue:
-        last_issue = issue[-1]
-        for i in range(len(issue)-2, -1, -1):
-            if last_issue == issue[i]:
-                del help[i]
-            else:
-                last_issue = issue[i]
-    help = list(q_help)
-    response_to_help = list()
-    for response in q_helpresponse:
-        response_to_help.append(response.helprequest)
-    help += response_to_help
-    help.sort()
-    if help:
-        last_help = help[-1]
-        for i in range(len(help)-2, -1, -1):
-            if last_help == help[i]:
-                del help[i]
-            else:
-                last_help = help[i]
-
-    return render_to_response("search/search_results.html", {'issues': issue, 'help_requests': help, "projects": projects}, context_instance=RequestContext(request))
+    json_comment = {}
+    comment_count = 0
+    for item in q_issuecomment:
+        json_comment["id-"+str(comment_count)] = item.issue.id
+        json_comment["summary-"+str(comment_count)] = item.issue.summary
+        json_comment["comment-"+str(comment_count)] = item.comment
+        comment_count = comment_count + 1
+    summary_count = 0
+    q_issuesummary = Issue.objects.filter(Q(summary__contains=search))
+    json_summary = {}
+    for item in q_issuesummary:
+        json_summary["id-"+str(summary_count)] = item.id
+        json_summary["summary-"+str(summary_count)] = item.summary
+        summary_count = summary_count + 1
+    q_issuedescription = Issue.objects.filter(Q(description__contains=search))
+    json_description = {}
+    description_count = 0
+    for item in q_issuedescription:
+        json_description["id-"+str(description_count)] = item.id
+        json_description["summary-"+str(description_count)] = item.summary
+        json_description["description-"+str(description_count)] = item.description
+        description_count = description_count + 1
+    q_helpname = HelpRequest.objects.filter(Q(name__contains=search))
+    json_name = {}
+    name_count = 0
+    for item in q_helpname:
+        json_name["id-"+str(name_count)] = item.id
+        json_name["name-"+str(name_count)] = item.name
+        name_count = name_count + 1
+    q_helpquestion = HelpRequest.objects.filter(Q(question__contains=search))
+    json_question = {}
+    question_count = 0
+    for item in q_helpquestion:
+        json_question["id-"+str(question_count)] = item.id
+        json_question["name-"+str(question_count)] = item.name
+        json_question["question-"+str(question_count)] = item.question
+        question_count = question_count + 1
+    print json_name
+    return render_to_response("search/search_results.html", {"search": search, "projects": projects, "comment_count": comment_count, "issue_comment": json.dumps(json_comment), "summary_count": summary_count, "issue_summary": json.dumps(json_summary), "description_count": description_count, "issue_description": json.dumps(json_description), "response_count": response_count, "help_response": json.dumps(json_response), "name_count": name_count, "help_name": json.dumps(json_name), "question_count": question_count, "help_question": json.dumps(json_question)}, context_instance=RequestContext(request))
