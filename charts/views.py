36ad2afc95c3129b527d70830ab0f3581069a0c7
jason <jason@jason-VirtualBox.(none)>
2013-08-02 14:35:54 -0700

    adding unassigned issues view

--- charts/views.py
+++ charts/views.py
@@ -123,7 +123,41 @@ def projects_chart(request):
 
 
 @login_required
+def unassigned_issues_chart(request):
+    try:
+        projects = Project.objects.all()
+    except:
+        print 'Unable to grab all projects'
+        projects = []
+
+    try:
+        users = User.objects.all()
+    except:
+        print "Can't get users"
+
+    try:
+        issues = Issue.objects.filter(assigned_to=None)
+        to_json_issues = []
+        for issue in issues:
+            json_issue = model_to_dict(issue)
+            json_issue['created'] = issue.created
+            json_issue['project_name'] = issue.project.name
+            for k,v in json_issue.items():
+                json_issue[k] = str(v)
+
+            to_json_issues.append(json_issue)
+    except Exception, e:
+        print e
+        print 'Unable to grab all Isssues'
+        to_json_issues = []
+
+    return render_to_response("charts/unassigned_issues_gantt_chart.html", {"projects": projects, "issues": json.dumps(to_json_issues).replace("'", r"\'"), "users": users}, context_instance=RequestContext(request))
+
+
+
+@login_required
 def issues_by_user_chart(request, user_id):
+    print 'user id is %s' % user_id
     try:
         users = User.objects.all()
     except:
