30d1319f31e5a698af85a18114a268fd71ac42b1
jason <jason@jason-VirtualBox.(none)>
2013-08-13 12:41:16 -0700

    fixing newsfeed where it shows ids to showing names

--- dashboard/views.py
+++ dashboard/views.py
@@ -13,30 +13,41 @@ def home(request):
     '''
         need comments
     '''
+    try:
+        users = User.objects.filter(is_staff=True)
+    except Exception, e:
+        print e
+        users = []
+
     #issues = Issue.objects.filter(assigned_to=request.user).order_by('-created')
     try:
         issues = Issue.objects.filter(Q(assigned_to=request.user) & (Q(status="active") | Q(status="retest") | Q(status="unverified") | Q(status__isnull=True))).order_by('-created')
     except Exception, e:
         print e
+        issues = []
     #pins = PinIssue.objects.select_related().filter(user=request.user)
     try:
         pins = Issue.objects.filter(pinissue__user=request.user).order_by('-created')
     except Exception, e:
         print e
+        pins = []
     try:
         subscribed = Issue.objects.filter(subscriptiontoissue__user=request.user).order_by('-created')
     except Exception, e:
         print e
+        subscribed = []
 
     try:
         newsfeeds = NewsFeedItem.objects.all().order_by('-id')[:20]
     except Exception, e:
         print e
+        newsfeeds = []
     #projects = Project.objects.filter(lead_developer=request.user).order_by('-created')
     try:
         projects = Project.objects.all()
     except Exception, e:
         print e
+        projects = []
 
     try:
         notification_recipients = NotificationRecipient.objects.select_related('notification').filter(user=request.user, read=False)
@@ -46,8 +57,10 @@ def home(request):
     except Exception, e:
         print e
         notification_recipients = None
+
+    print users
     #subscribed = SubscriptionToIssue.objects.select_related().filter(user=request.user)
     return render_to_response("dashboard/dashboard.html", {"issues": issues, "subscribed": subscribed,
             "projects": projects, "pins": pins, "newsfeeds": newsfeeds, "notifications": notification_recipients,
-            "num_notifications": num_notifications, "page_type": "Dashboard", "page_value": "Overview",
+            "num_notifications": num_notifications, "users": users, "page_type": "Dashboard", "page_value": "Overview",
             "navIndicator": 'dashboard'}, context_instance=RequestContext(request))
