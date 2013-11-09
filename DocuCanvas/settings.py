7c17648d796b8b3590321802c14a10dadc0b8df0
Tyler Agee <tyler@pyroturtle.com>
2013-11-09 14:03:13 -0800

    fixing communication celery import

--- DocuCanvas/settings.py
+++ DocuCanvas/settings.py
@@ -212,7 +212,7 @@ djcelery.setup_loader()
 ### given the prevelance of heroku usage, this is just an easy way to use that and fall back to localhost for dev
 BROKER_URL = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 CELERY_RESULT_BACKEND = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
-CELERY_IMPORTS = ("issues.email", "accounts.views")  # Uncomment line to add where the tasks are! A suggest default is in there
+CELERY_IMPORTS = ("communications.views", "accounts.views")  # Uncomment line to add where the tasks are! A suggest default is in there
 #CELERYBEAT_SCHEDULE = {'update_index': {'task': 'haystack.update_index.update_index', 'schedule': timedelta(days=1), },}
 ### /CELERY ###
 
